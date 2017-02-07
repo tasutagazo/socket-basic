7f30a308f03f11959a2b7e616eb56bb0cc4047fc
Toby Paul <tasutagazo@gmail.com>
2017-02-08 02:13:07 +0800

    added @currentUsers

--- server.js
+++ server.js
@@ -9,13 +9,37 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
-io.on('connection', function(socket){
+//sends current user to socket
+function sendCurrentUsers(socket){
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined'){
+		return;
+	} 
+
+	Object.keys(clientInfo).forEach(function (socketId){
+		var userInfo = clientInfo[socketId];
+
+		if(info.room === userInfo.room){
+			users.push(userInfo.name);
+		}
+		socket.emit('message', {
+			name: 'System',
+			text: 'Current Users: ' + users.join(', '),
+			timestamp: moment().valueOf()
+		})
+	});
+
+}
+
+io.on('connection', function(socket) {
 	console.log('user connected via socket.io!');
 
 
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		var userData = clientInfo[socket.id];
-		if(typeof clientInfo[socket.id] !== 'undefined'){
+		if (typeof clientInfo[socket.id] !== 'undefined') {
 			socket.leave(userData.room);
 			io.to(userData.room).emit('message', {
 				name: 'System',
@@ -25,20 +49,25 @@ io.on('connection', function(socket){
 			delete clientInfo[socket.id];
 		}
 	});
-	socket.on('joinRoom', function(req){
+	socket.on('joinRoom', function(req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
 		socket.broadcast.to(req.room).emit('message', {
 			name: 'System',
 			text: req.name + ' has joined',
 			timestamp: moment().valueOf()
-		});	
+		});
 	});
 
-	socket.on('message', function(message){
+	socket.on('message', function(message) {
 		console.log('Message received ' + message.text);
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	//timestamp property - Javascript timestamp 
@@ -46,12 +75,11 @@ io.on('connection', function(socket){
 
 	socket.emit('message', {
 		name: 'System',
-		text: 'Welcome to the chat app', 
+		text: 'Welcome to the chat app',
 		timestamp: moment().valueOf()
 	});
 });
 
-http.listen(PORT, function(){
+http.listen(PORT, function() {
 	console.log('server started');
 })
-
