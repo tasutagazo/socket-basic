d1f01c44f727c86882fbe68db78d146e642a570e
Toby Paul <tasutagazo@gmail.com>
2017-02-07 15:37:48 +0800

    moment-example use

---
+++ moment-example.js
@@ -0,0 +1,19 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+
+now.subtract(1, 'year');
+// console.log(now.format('X'));
+// console.log(now.format('x'));
+// console.log(now.valueOf());
+
+var timestamp = 1454830121331;
+var timestampMoment = moment.utc(timestamp); 
+
+
+
+console.log(timestampMoment.local().format('h:mm a'));
+
+// console.log(now.format());
+// console.log(now.format('Do MMM YYYY, h:mma')); //7th Feb 2017 3.12pm
